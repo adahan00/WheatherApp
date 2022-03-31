5b3be0f7a09067913e40e83d077f7e7bb6ef4eb5
adahan00 <adahankasymaliev6@gmail.com>
2022-03-31 19:42:25 +0300

    rehs

---
+++ WheatherApp/app/src/main/java/kg/geektech/wheatherapp/common/Resource.java
@@ -0,0 +1,32 @@
+package kg.geektech.wheatherapp.common;
+
+import androidx.annotation.NonNull;
+
+public class Resource {
+    @NonNull
+    public final Status status;
+    public final T data;
+    public final String msg;
+
+    public Resource(@NonNull Status status, T data, String msg) {
+        this.status = status;
+        this.data = data;
+        this.msg = msg;
+    }
+
+    public static <T> Resource<T> success(@NonNull T data) {
+        return new Resource<>(Status.SUCCESS, data, null);
+
+    }
+
+    public static <T> Resource<T> error(String msg, @NonNull T data) {
+        return new Resource<>(Status.ERROR, data, msg);
+    }
+
+    public static <T> Resource<T> loading() {
+        return new Resource<>(Status.LOADING, null, null);
+    }
+
+}
+
+}
